38113a06c3d59f50179670e4cd96e47c1f67131e
kinjelom <mariusz@olejnik.net>
2025-08-26 20:00:56 +0200

    go 1.24, coredns 1.12.3, recursor 1.4.0

--- vendor-golang.sh
+++ vendor-golang.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bosh vendor-package golang-1.23-linux ../../../bosh-packages/bosh-package-golang-release
+bosh vendor-package golang-1.24-linux ../../../bosh-packages/bosh-package-golang-release
